f13afb04dfb455cc57808a628b18b1451e9ac873
traviswhittington88 <50853094+traviswhittington88@users.noreply.github.com>
2019-06-17 22:33:30 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+$(function(){
+  $(".js-shopping-list-form").submit(function(event){
+    event.preventDefault();
+    const item = $(this).find('input[name="shopping-list-entry"]').val();
+    $(".shopping-list").append("<li>" + [item] + "</li>");
+  
+  });
+};
