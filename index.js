85a0bf9ba500c6414baa0b863cba286d690924df
traviswhittington88 <50853094+traviswhittington88@users.noreply.github.com>
2019-06-18 12:59:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ $(function(){
   $(".js-shopping-list-form").submit(function(event){
     event.preventDefault();
     const item = $(this).find('input[name="shopping-list-entry"]').val();
-    $(".shopping-list").append("<li>" + [item] + "</li>");
-  
+    $(".shopping-list").append(`"<li>" + ${item} + "</li>"`);
+   
+    
   });
 };
